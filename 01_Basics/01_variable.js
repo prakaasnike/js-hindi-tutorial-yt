c3a62958bb8811d91f2f56f031c3d2593dc51a47
Prakash Nayak <erprakashnayak@gmail.com>
2024-01-15 22:13:47 +0545

    variable

---
+++ 01_Basics/01_variable.js
@@ -0,0 +1,19 @@
+const accountId = 12123123
+let accountEmail = "account@gmail.com"
+var accountPassword = "password"
+accountCity = "Kathmandu"
+let accountState;
+
+// accountId = 23 // no allowed
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+
+
+
+
+/*
+Prefer not to use var
+issue with scope
+*/
+
+
