4b58ccdfb48c8aad18f206c6784b260fec9855de
Prakash Nayak <erprakashnayak@gmail.com>
2024-01-16 12:05:57 +0545

    comparisions

---
+++ 01_Basics/05_comparison.js
@@ -0,0 +1,66 @@
+/* ------------------------------- Equal (==): ------------------------------ */
+let x = 5;
+let y = '5';
+console.log(x == y); // true (loose equality)
+
+
+/* --------------------------- Strict Equal (===): -------------------------- */
+let a = 5;
+let b = '5';
+console.log(a === b); // false (strict equality)
+
+/* ----------------------------- Not Equal (!=): ---------------------------- */
+let c = 5;
+let d = '5';
+console.log(c != d); // false (loose inequality)
+
+/* ------------------------- Strict Not Equal (!==): ------------------------ */
+let s = 5;
+let t = '5';
+console.log(s !== t); // true (strict inequality)
+
+/* ------------------------------ Greater Than ------------------------------ */
+let g = 10;
+let e = 5;
+console.log(g > e); // true
+/* -------------------------------------------------------------------------- */
+
+/* -------------------------------- less than ------------------------------- */
+let l = 10;
+let tt = 5;
+console.log(l < tt); // false
+
+
+/* -------------------------- greater than equal to ------------------------- */
+let xx = 10;
+let yy = 10;
+console.log(xx >= yy); // true
+
+
+/* ----------------------------- less than equal ---------------------------- */
+let xcx = 10;
+let xcy = 10;
+console.log(xcx <= xcy); // true
+
+/* ------------------------------- logical && ------------------------------- */
+let sdx = 5;
+let sdy = 10;
+console.log(sdx < 10 && sdy > 5); // true
+
+/* ------------------------------- logical or ------------------------------- */
+let lox = 5;
+let loy = 10;
+console.log(lox < 5 || loy > 5); // true
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined > 0);
+
+
+console.log("2" === 2); //strict checking
+
+
