cbcbdfbb1543f08d4d4ef349b725707d494419aa
Prakash Nayak <erprakashnayak@gmail.com>
2024-01-16 09:58:40 +0545

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+'use strict'; //treat all js code as newer version
+
+// alert(3+3) works in browser not with node js
+
+let name = "prakash"
+let age = 23
+let isLoggedIn = false
+
+
+// number => 1 to 100
+//bigINt
+//string => single, double quote
+//boolean => true or false
+//null => empty value
+//undefined => let stay;
+
+//object
+
+console.log(typeof "string");
+console.log(typeof null);
